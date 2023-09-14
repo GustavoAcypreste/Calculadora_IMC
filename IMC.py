5ab282276ae25c06dcec5959b3a9b82a0f35f208
GustavoAcypreste <gustavoacyprestemr@gmail.com>
2023-09-14 20:49:25 -0300

    Qualidade do IMC

--- IMC.py
+++ IMC.py
@@ -6,4 +6,16 @@ altura = input("Digite a sua altura em metros: ")
 print(nome, idade, sep=" ")
 imc = (float(peso)/ (float(altura) ** 2))
 print(imc)
-print(int(imc))
+
+abaixo = (float(imc) < 18)
+normal = (float(imc) >= 18 and float(imc)< 25)
+acima = (float(imc) >= 25)
+
+if normal:
+    print("seu IMC está normal")
+
+if acima: 
+    print("Seu IMC está acima do desejado")
+
+if abaixo:
+    print("seu IMC está abaixo do desejado")
