8dde1c08f6b4dca899a6c05dab0db393d3457a2d
GustavoAcypreste <gustavoacyprestemr@gmail.com>
2023-09-14 20:09:58 -0300

    teste

---
+++ print_input.py
@@ -0,0 +1,8 @@
+nome = input("Informe o seu nome: ")
+idade = input("Infomre sua idade: ")
+peso = input("Informe o seu peso em kg: ")
+
+print(nome, idade, peso)
+print(nome, idade, peso, end="...\n")
+print(nome, idade, peso, sep="#", end="...\n" )
+print(nome, idade, peso, sep="#")
